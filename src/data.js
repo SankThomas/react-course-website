89ac32ecd7ba9c78e9f11627c30fd6e99687c262
Thomas Sankara <thomassankara74@gmail.com>
2021-05-14 12:23:26 +0300

    dummy data array

---
+++ src/data.js
@@ -0,0 +1,64 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const data = [
+  {
+    id: uuidv4(),
+    title: "Birthday App",
+    image: "./assets/birthday-app.png",
+    link: "https://tsbdesigns-birthday-app.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "Accordion / FAQs section",
+    image: "./assets/accordion.png",
+    link: "https://tsbdesigns-accordion.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "Profile Read More / Conditional Rendering",
+    image: "./assets/profile-read-more.png",
+    link: "https://tsbdesigns-profile-read-more.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "Navbar / Conditional Rendering",
+    image: "./assets/navbar.png",
+    link: "https://tsbdesigns-navbar.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "Launch Timer",
+    image: "./assets/launch-timer.png",
+    link: "https://tsbdesigns-launch-timer.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "Nexus Todo App",
+    image: "./assets/nexus.png",
+    link: "https://tsbdesigns-nexus.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "Random Meal Generator",
+    image: "./assets/meal-generator.png",
+    link: "https://tsbdesigns-meal-generator.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "Random User App",
+    image: "./assets/random-user-app.png",
+    link: "https://tsbdesigns-random-user-app.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "Countries App",
+    image: "./assets/countries-app.png",
+    link: "https://tsbdesigns-countries-app.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "Personal Blog",
+    image: "./assets/blog.png",
+    link: "https://tsbdesigns-blog.netlify.app",
+  },
+]
