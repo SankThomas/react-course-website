a1759ece8217780a1683d38ff0c25e1586b5f6d0
Thomas Sankara <thomassankara74@gmail.com>
2021-09-10 11:55:33 +0300

    data

---
+++ src/data/data.js
@@ -0,0 +1,94 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const data = [
+  {
+    id: uuidv4(),
+    title: "Contact List App",
+    image: "./assets/contact-list-app.png",
+    link: "https://tsbdesigns-birthday-app.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "Accordion / FAQs section",
+    image: "./assets/accordion.png",
+    link: "https://tsbdesigns-accordion.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "Profile Read More / Conditional Rendering",
+    image: "./assets/profile-read-more.png",
+    link: "https://tsbdesigns-profile-read-more.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "Navbar / Conditional Rendering",
+    image: "./assets/navbar.png",
+    link: "https://tsbdesigns-navbar.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "Launch Timer",
+    image: "./assets/launch-timer.png",
+    link: "https://tsbdesigns-launch-timer.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "YouTube Comments",
+    image: "./assets/youtube-comments.png",
+    link: "https://tsbdesigns-youtube-comments.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "Random Meal Generator",
+    image: "./assets/meal-generator.png",
+    link: "https://tsbdesigns-meal-generator.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "Random User App",
+    image: "./assets/random-user-app.png",
+    link: "https://tsbdesigns-random-user-app.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "Countries App",
+    image: "./assets/countries-app.png",
+    link: "https://tsbdesigns-countries-app.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "Personal Blog",
+    image: "./assets/blog.png",
+    link: "https://tsbdesigns-blog.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "Password Generator",
+    image: "./assets/password-generator.png",
+    link: "https://tsbdesigns-password-generator.netlify.app",
+  },
+  // {
+  //   id: uuidv4(),
+  //   title: "The Mad Chatter",
+  //   image: "./assets/pwa.png",
+  //   link: "https://tsbdesigns-pwa.png",
+  // },
+  {
+    id: uuidv4(),
+    title: "Electron Desktop App",
+    image: "./assets/electron.png",
+    link: "https://tsbdesigns-electron.png",
+  },
+  {
+    id: uuidv4(),
+    title: "Framer Motion",
+    image: "./assets/framer-motion.png",
+    link: "https://tsbdesigns-framer-motion.netlify.app",
+  },
+  {
+    id: uuidv4(),
+    title: "Dashboard",
+    image: "./assets/dashboard.png",
+    link: "https://tsbdesigns-dashboard.netlify.app",
+  },
+]
