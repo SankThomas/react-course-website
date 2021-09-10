5376d67ea43874a8a517cfaebd49adbdbaaafa20
Thomas Sankara <thomassankara74@gmail.com>
2021-09-10 11:55:11 +0300

    Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="py-10">
+        <div className="text-center text-gray-500 md:text-lg">
+          Built with ❤. (Technically ReactJs)
+        </div>
+      </footer>
+    </>
+  )
+}
