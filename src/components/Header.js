4ff36083ab14c3aaaed11ee9b1321163ee88000a
Thomas Sankara <thomassankara74@gmail.com>
2021-09-10 22:57:28 +0300

    reduce padding around the header, reduce margin top on nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,14 @@ import Button from "./Button"
 export default function Header() {
   return (
     <>
-      <header className="flex flex-col items-center justify-center p-5 sm:flex-row sm:justify-between lg:px-10 fixed w-full shadow bg-white top-0 left-0">
+      <header className="flex flex-col items-center justify-center p-3 sm:flex-row sm:justify-between lg:px-10 fixed w-full shadow bg-white top-0 left-0">
         <div>
           <h2 className="font-bold text-4xl">
             <a href="https://react-course-website.netlify.app">Sankara</a>
           </h2>
         </div>
 
-        <nav className="mt-3 sm:mt-0">
+        <nav className="mt-2 sm:mt-0">
           <ul className="flex items-center">
             <li className="mr-3">
               <a href="https://udemy.com/thomas-sankara-babu">Discussions</a>
