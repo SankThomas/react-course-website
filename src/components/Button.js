a6a1d886b8ee2ab3e580e967b134bcaad0c4cf75
Thomas Sankara <thomassankara74@gmail.com>
2021-09-10 11:54:51 +0300

    Button component

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+export default function Button() {
+  return (
+    <>
+      <a
+        href="https://udemy.com/thomas-sankara-babu"
+        className="bg-blue-400 py-1 px-2 rounded shadow border border-blue-400 hover:transition-all duration-300 hover:bg-blue-200 hover:border-blue-200"
+      >
+        Start The Course
+      </a>
+    </>
+  )
+}
