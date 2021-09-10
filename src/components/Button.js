22263e6c2019ff641b5fd08ba5f5b85ec633feb3
Thomas Sankara <thomassankara74@gmail.com>
2021-09-10 22:56:52 +0300

    increase top padding

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ export default function Button() {
     <>
       <a
         href="https://udemy.com/thomas-sankara-babu"
-        className="bg-blue-400 py-1 px-2 rounded shadow border border-blue-400 hover:transition-all duration-300 hover:bg-blue-200 hover:border-blue-200"
+        className="bg-blue-400 pt-2 pb-1 px-2 rounded shadow border border-blue-400 hover:transition-all duration-300 hover:bg-blue-200 hover:border-blue-200"
       >
         Start The Course
       </a>
