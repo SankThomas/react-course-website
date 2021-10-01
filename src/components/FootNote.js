67e59acdb44d31bef32d2ec5cfba6f019c603e76
Thomas Sankara <thomassankara74@gmail.com>
2021-10-01 15:47:09 +0300

    add dynamic number on footnote

--- src/components/FootNote.js
+++ src/components/FootNote.js
@@ -1,4 +1,9 @@
+import { useState } from "react"
+import { data } from "../data/data"
+
 export default function FootNote() {
+  const [items] = useState(data)
+
   return (
     <>
       <section className="text-gray-500 md:text-lg pt-10 lg:max-w-4xl lg:mx-auto">
@@ -7,12 +12,12 @@ export default function FootNote() {
           build user experiences.
         </p>
         <p>
-          Thank you for buying the course. In the 15 projects we will cover the
-          building blocks of React from components to props to state management,
-          and steadily make our way up to more and more advanced concepts like
-          passing down props, making network requests and finalize everything
-          with the context api - which was the hardest thing for me to learn in
-          React. I hope this course makes it easier for you.
+          Thank you for buying the course. In the {items.length} projects we
+          will cover the building blocks of React from components to props to
+          state management, and steadily make our way up to more and more
+          advanced concepts like passing down props, making network requests and
+          finalize everything with the context api - which was the hardest thing
+          for me to learn in React. I hope this course makes it easier for you.
         </p>
       </section>
     </>
